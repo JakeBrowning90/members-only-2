f8cd92dfe488af1b226e3db6cc68b7ec5142af74
JakeBrowning90 <jacobbrowning90@gmail.com>
2024-11-07 15:43:43 -0500

    Add basic signup and login functions

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,64 @@
+const { body, validationResult } = require("express-validator");
+const db = require("../db/queries");
 const asyncHandler = require("express-async-handler");
+const passport = require("passport");
+const bcrypt = require("bcryptjs");
+const LocalStrategy = require("passport-local").Strategy;
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await db.getUserByEmail(username);
+      if (!user) {
+        return done(null, false, { message: "Incorrect email" });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.getUserById(id);
+    return done(null, user);
+  } catch (err) {
+    return done(err);
+  }
+});
+
+const validateForm = [
+  body("username")
+    .trim()
+    .isLength({ min: 1, max: 20 })
+    .withMessage("Username must contain between 1 and 20 characters."),
+  // TODO: check for existing email
+  body("email")
+    .trim()
+    .isEmail()
+    .withMessage("Invalid email address format.")
+    .isLength({ min: 1, max: 50 })
+    .withMessage("Email must contain between 1 and 50 characters."),
+  body("password")
+    .trim()
+    .isLength({ min: 1, max: 20 })
+    .withMessage("Password must contain between 1 and 20 characters."),
+  body("passwordConfirm")
+    .trim()
+    .custom((value, { req }) => {
+      return value === req.body.password;
+    })
+    .withMessage("Typed passwords do not match."),
+];
 
 exports.getIndex = asyncHandler(async (req, res) => {
   res.render("index");
@@ -8,20 +68,55 @@ exports.getSignup = asyncHandler(async (req, res) => {
   res.render("signup");
 });
 
-exports.postSignup = asyncHandler(async (req, res) => {
-  res.render("index");
-});
+exports.postSignup = [
+  validateForm,
+  asyncHandler(async (req, res) => {
+    const user = req.body;
+    const errors = validationResult(req);
+    // If errors, rerender form and display errors
+    if (!errors.isEmpty()) {
+      // TODO: Render with errors
+      res.render("signup", { errors: errors.array() });
+    } else {
+      // If valid, insert user and redirect to login
+      try {
+        bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+          if (err) {
+            res.redirect("/sign-up");
+          } else {
+            await db.insertUser(
+              req.body.username,
+              req.body.email,
+              hashedPassword
+            );
+            res.redirect("/login");
+          }
+        });
+      } catch (err) {
+        return next(err);
+      }
+    }
+  }),
+];
 
 exports.getLogin = asyncHandler(async (req, res) => {
   res.render("login");
 });
 
-exports.postLogin = asyncHandler(async (req, res) => {
-  res.render("index");
-});
+exports.postLogin = [
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "login",
+  }),
+];
 
 exports.getLogout = asyncHandler(async (req, res) => {
-  res.render("index");
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
 });
 
 exports.getMembership = asyncHandler(async (req, res) => {
--- db/queries.js
+++ db/queries.js
@@ -4,4 +4,26 @@ const pool = require("./pool");
 //     return values;
 // }
 
-module.exports = {};
+async function insertUser(username, email, password) {
+  await pool.query(
+    "INSERT INTO users (username, email, password, is_member, is_admin) VALUES ($1, $2, $3, false, false)",
+    [username, email, password]
+  );
+}
+
+async function getUserByEmail(email) {
+  console.log(email);
+  const { rows } = await pool.query("SELECT * FROM users WHERE email = ($1)", [
+    email,
+  ]);
+  return rows[0];
+}
+
+async function getUserById(id) {
+  const { rows } = await pool.query("SELECT * FROM users WHERE id = ($1)", [
+    id,
+  ]);
+  return rows[0];
+}
+
+module.exports = { insertUser, getUserByEmail, getUserById };
